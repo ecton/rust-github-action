22266b24ed697f7a5f6c0c3ca0747e2824032b5c
Jonathan Johnson <jon@khonsulabs.com>
2021-02-21 15:36:07 -0800

    Adding sass compilation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ sed -i s/CACHEBUSTER/`uuidgen`/g static/bootstrap.html
 BASWS_CLIENT_ENCRYPTION_KEY=$1 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/release/web.wasm --target web --out-dir static/pkg --out-name web --remove-producers-section
 wasm-opt -Os static/pkg/web_bg.wasm -o static/pkg/web_bg.wasm
+sass sass/styles.sass static/styles.css
 
 cd ../native
 cargo build --bin cosmicverge-server --release
