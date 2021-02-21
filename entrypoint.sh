2ac666d4ef3b66b8276a4f73038ef3a9d550b1df
Jonathan Johnson <jon@khonsulabs.com>
2021-02-21 14:58:03 -0800

    Initial commit.

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+cd web
+sed -i s/CACHEBUSTER/`uuidgen`/g static/bootstrap.html
+BASWS_CLIENT_ENCRYPTION_KEY=$1 cargo build --target wasm32-unknown-unknown --release
+wasm-bindgen target/wasm32-unknown-unknown/release/web.wasm --target web --out-dir static/pkg --out-name web --remove-producers-section
+wasm-opt -Os static/pkg/web_bg.wasm -o static/pkg/web_bg.wasm
+
+cd ../native
+cargo build --bin cosmicverge-server --release
+cargo run --bin cosmicverge-server --release -- generate-assets ../web/static/
